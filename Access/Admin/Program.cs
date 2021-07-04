764f76e6b1cc07e458477a5f03d9f6289381aef0
jdc20181 <jdc20181@gmail.com>
2021-07-03 22:46:10 -0400

    Add files via upload

---
+++ Access/Admin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IT_launchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Business OS IT Admin Shell V. 0.1 Alpha");
+
+            Console.WriteLine("A list of options are coming soon");
+            Console.WriteLine("For now...");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
